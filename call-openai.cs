be4b1ee06974cdf6d1755263761b49fcf88057bf
Mark <34512169+fireblade95402@users.noreply.github.com>
2024-09-18 14:35:56 +0000

    initial

--- call-openai.cs
+++ call-openai.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using Azure.AI.OpenAI;
+using Azure.AI.OpenAI.Chat;
+using OpenAI.Chat;
 using System;
 using static System.Environment;
 using Azure;
@@ -20,18 +22,16 @@ namespace Company.Function
     {
 
         // Create a class to hold the chat messages
-        private class ChatMessage
+        private class ChatMessageTemp
         {
-            public ChatMessage(string role, string text, string image = "")
+            public ChatMessageTemp(string role, string text)
             {
                 Role = role;
                 Text = text;
-                Image = image;
             }
 
             public string Role { get; set; }
             public string Text { get; set; }
-            public string Image { get; set; }
         }
 
         //create logger
@@ -46,6 +46,7 @@ namespace Company.Function
         [Function("call_openai")]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
+#pragma warning disable AOAI001 // Suppress the diagnostic warning
 
             // Get the environment variables for the OpenAI endpoint, key and model
             string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", null, true, true);
@@ -54,55 +55,61 @@ namespace Company.Function
             string user_message = GetEnvironmentVariable("AZURE_OPENAI_USER_MESSAGE", null, true, true);
             int max_tokens = Convert.ToInt32(GetEnvironmentVariable("AZURE_OPENAI_MAX_TOKENS", null, true, true));
             double temperature = Convert.ToDouble(GetEnvironmentVariable("AZURE_OPENAI_TEMPERATURE", "0.9", true, true));
-            bool image_generation = Convert.ToBoolean(GetEnvironmentVariable("AZURE_OPENAI_IMAGE_GENERATION", "false", true, true));
+
+            string searchEndpoint = GetEnvironmentVariable("SEARCH_ENDPOINT", null, true, true);
+            string searchKey = GetEnvironmentVariable("SEARCH_KEY", null, true, true);
+            string searchIndex = GetEnvironmentVariable("SEARCH_INDEX", null, true, true);
 
 
             // Create a list of messages to send to the OpenAI chat endpoint
-            List<ChatMessage> messages = new List<ChatMessage>();
+            List<UserChatMessage> messages = new List<UserChatMessage>();
+            List<ChatMessageTemp> messagesTemp = new List<ChatMessageTemp>();
 
             // Get the question from the query string
             var chat_question = req.Query["question"] ?? user_message;
 
             // Get the system message from the query string
-            var system_message = req.Query["system_message"] ?? GetEnvironmentVariable("AZURE_OPENAI_SYSTEM_MESSAGE", null, true, true);
+            var system_prompt = req.Query["system_prompt"] ?? GetEnvironmentVariable("AZURE_OPENAI_SYSTEM_MESSAGE", null, true, true);
 
             // Get the user from the query string
             var user = req.Query["user"] ?? "user";
 
-            //Example of how to get the headers
-            //request headers 
-            // var headers = req.Headers;
-            // //get Host from headers
-            // var host = headers.GetValues("AZURE_OPENAI_SYSTEM_MESSAGE").FirstOrDefault() ;
-            
+            //Get Search Index from the query string
+            searchIndex = req.Query["search_index"] ?? searchIndex;
+
             //Log the question and user
-            _logger.LogInformation($"System Message: {system_message}");
+            _logger.LogInformation($"System Prompt: {system_prompt}");
             _logger.LogInformation($"User: {user}");
             _logger.LogInformation($"Question: {chat_question}");
-
+            _logger.LogInformation($"Search Index: {searchIndex}");
 
             // Get the request body for the chat history
             string? requestBody = await req.ReadAsStringAsync();
-          
-
-            // Create the chat options
-            ChatCompletionsOptions? chatCompletionsOptions = null;
-
             // If the request body is empty, then this is the first request
             if (string.IsNullOrEmpty(requestBody))
             {
-                messages.Add(new ChatMessage("system", system_message));
+                messages.Add(new UserChatMessage("system", system_prompt));
+                messagesTemp.Add(new ChatMessageTemp("system", system_prompt));
             }
             else
             {
                 // Deserialize the request body into a list of messages
                 try
-                {   
-                    messages = JsonConvert.DeserializeObject<List<ChatMessage>>(requestBody) ?? new List<ChatMessage>();
+                {
+                    messagesTemp = JsonConvert.DeserializeObject<List<ChatMessageTemp>>(requestBody) ?? new List<ChatMessageTemp>();
 
                     // Check if the list of messages is empty
-                    if (messages.Count == 0){
+                    if (messagesTemp.Count == 0)
+                    {
                         throw new Exception($"Error with loading history");
+                    }
+                    else
+                    {
+                        // Loop through the messages and add them to the list of messages to send to the OpenAI chat endpoint
+                        foreach (ChatMessageTemp message in messagesTemp)
+                        {
+                            messages.Add(new UserChatMessage(message.Role, message.Text));
+                        }
                     }
                 }
                 catch (Exception ex)
@@ -114,74 +121,69 @@ namespace Company.Function
 
             // Create the chat options
             // Loop through the messages and add them to the list of messages to send to the OpenAI chat endpoint
-            chatCompletionsOptions = new ChatCompletionsOptions();
-            foreach (ChatMessage message in messages)
-            {
-                chatCompletionsOptions.Messages.Add(new Azure.AI.OpenAI.ChatMessage(message.Role, message.Text));
-            }
+            ChatCompletionOptions options = new ChatCompletionOptions();
+            // foreach (ChatMessage2 message in messages)
+            // {
+            //     aimessages.Add(new UserChatMessage(message.Role, message.Text));
+            // }
             // Add the question to the list of messages to send to the OpenAI chat endpoint
-            messages.Add(new ChatMessage("user", chat_question));
-            chatCompletionsOptions.Messages.Add(new Azure.AI.OpenAI.ChatMessage(Azure.AI.OpenAI.ChatRole.User, chat_question));
+            messages.Add(new UserChatMessage("user", chat_question));
+            messagesTemp.Add(new ChatMessageTemp("user", chat_question));
 
             // Set the chat options
-            chatCompletionsOptions.MaxTokens = max_tokens;
-            chatCompletionsOptions.User = user;
-            chatCompletionsOptions.Temperature = (float)temperature;
+            options.MaxTokens = max_tokens;
+            options.EndUserId = user;
+            options.Temperature = (float)temperature;
+
+            //add extrabody to the chat options
+            options.AddDataSource(new AzureSearchChatDataSource()
+            {
+                Endpoint = new Uri(searchEndpoint),
+                IndexName = searchIndex,
+                Authentication = DataSourceAuthentication.FromApiKey(searchKey), // Add your Azure AI Search admin key here
+            });
+
 
             // Create the OpenAI client
-            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
-
-            // Call the OpenAI chat endpoint
-            Response<ChatCompletions> chat_response = null;
-            try
-            {
-            chat_response = client.GetChatCompletions(
-                            deploymentOrModelName: model,
-                            chatCompletionsOptions);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"GetChatCompletions Error: {ex.Message}");
-                throw new Exception($"GetChatCompletions Error: {ex.Message}");
-            }
-
-            // Loop through the responses and generate the images and add them to the list of messages
-            foreach (var resp_message in chat_response.Value.Choices)
-            {
-                _logger.LogInformation($"Response: {resp_message.Message.Content}");
-            
-
-                //Generate Image
-                string imageUri = "";
-                if (image_generation == true)
+            AzureKeyCredential credential = new(key); // Add your OpenAI API key here
+            AzureOpenAIClient azureClient = new(
+                new Uri(endpoint),
+                credential
+            );
+            ChatClient client = azureClient.GetChatClient(model);
+            ChatCompletion completion = client.CompleteChat(
+                messages,
+                new ChatCompletionOptions
                 {
-                    Response<ImageGenerations> imageGenerations = await client.GetImageGenerationsAsync(
-                        new ImageGenerationOptions()
-                        {
-                            Prompt = chat_question + "? " +  resp_message.Message.Content.ToString(),
-                            Size = ImageSize.Size512x512,
-                        });
-
-                    // Image Generations responses provide URLs you can use to retrieve requested images
-                    imageUri = imageGenerations.Value.Data[0].Url.ToString();
-
-                    _logger.LogInformation($"Image: {imageUri}");
+                    //PastMessages = 10,
+                    Temperature = (float)0.7,
+                    TopP = (float)0.95,
+                    FrequencyPenalty = (float)0,
+                    PresencePenalty = (float)0,
+                    MaxTokens = 1500,
+                    //StopSequences = new List<string>(),
                 }
-                // Add the response to the list of messages
-                messages.Add(new ChatMessage(resp_message.Message.Role.ToString(), resp_message.Message.Content, imageUri));
+            );
 
+            foreach (var resp_message in completion.Content)
+            {
+                //messages.Add(new UserChatMessage("assistant", resp_message.Text));
+                messagesTemp.Add(new ChatMessageTemp("assistant", resp_message.Text));
             }
 
-            // Convert the list of messages to JSON
-            var jsonToReturn = JsonConvert.SerializeObject(messages);
+
+            // Process and print the response
+            var jsonToReturn = JsonConvert.SerializeObject(messagesTemp);
 
             // Return the JSON
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "application/json; charset=utf-8");
             response.WriteString(jsonToReturn);
             return response;
-        }
+#pragma warning restore AOAI001 // Restore the diagnostic warning
 
+        }
+        
         // Get the environment variable
         public static string GetEnvironmentVariable(string name, string? defaultValue = null, bool throwIfNotFound = false, bool throwIfEmpty = false)
         {
